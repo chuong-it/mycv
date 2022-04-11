6a151183e4b7f7a2147cd58ffcfabb4c3e642750
chuong <chuong.nguyen@bnksolution.com>
2022-04-11 10:41:37 +0700

    [Login chuongNB]

---
+++ src/components/login/Login.js
@@ -0,0 +1,13 @@
+import "./Login.css";
+function Login() {
+    return(
+        <div className="login">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+export default Login;
